f0eebb12e3787279fb77799bd107c5f4aa8756c8
baekcii <bsy8470@naver.com>
2026-04-16 01:08:52 +0900

    feat: Connection 제공 객체 추가

---
+++ src/main/kotlin/database/DatabaseConfig.kt
@@ -0,0 +1,12 @@
+package database
+
+import java.sql.Connection
+import java.sql.DriverManager
+
+object DatabaseConfig {
+    private const val LOCAL_URL = "jdbc:h2:~/test"
+
+    fun getConnection(url: String = LOCAL_URL): Connection {
+        return DriverManager.getConnection(url, "sa", "")
+    }
+}
